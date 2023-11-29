20f9f6342e352567224d8fa59758592a6cfc2044
Rai Joia <megac154@gmail.com>
2023-11-29 16:59:22 -0300

    next(component): realocado box

---
+++ src/components/Box/Box.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { BaseComponent } from '@src/theme/BaseComponent';
+import { StyleSheet } from '@src/theme/StyleSheet';
+
+interface BoxProps {
+  tag?: any;
+  children?: React.ReactNode;
+  styleSheet?: StyleSheet;
+}
+
+export default function Box({ children, styleSheet, tag, ...props }: BoxProps) {
+  const Tag = tag || 'div';
+  return (
+    <BaseComponent as={Tag} styleSheet={styleSheet} {...props}>
+      {children}
+    </BaseComponent>
+  );
+}
