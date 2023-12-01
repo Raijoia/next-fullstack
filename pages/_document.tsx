8a58de096fa064f831a867ce395204cf2d766fc9
Rai Joia <megac154@gmail.com>
2023-12-01 12:09:30 -0300

    next(font): adicionando fonte

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,11 @@
-import type { DocumentContext, DocumentInitialProps } from 'next/document';
-import Document from 'next/document';
+import Document, {
+  Html,
+  Main,
+  Head,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps
+} from 'next/document';
 
 import { ServerStyleSheet } from 'styled-components';
 
@@ -25,5 +31,24 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
