5f603f2d782e56b4ab753dfb18af57ed5e0b4890
Raí Joia <megac154@gmail.com>
2023-12-14 15:23:54 -0300

    next(post): PostsService criado,  markdown para json

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
+import PostsService from '@src/services/posts/PostsService';
 import { withConfig } from '@src/services/template/withConfig';
 
 export { default } from '@src/screens/HomeScreen/HomeScreen';
 
 export async function getStaticProps() {
+  const posts = await PostsService().getAll();
+
   return {
-    props: await withConfig({})
+    props: await withConfig({
+      posts
+    })
   };
 }
---
+++ src/services/posts/PostsService.ts
@@ -0,0 +1,44 @@
+import fs from 'fs/promises';
+import matter from 'gray-matter';
+import path from 'path';
+
+export interface Post {
+  metadata: {
+    date: string;
+    url: string;
+    excerpt: string;
+    tags: string[];
+  };
+  slug: string; // como se fosse o id do post
+  title: string;
+  content: string;
+}
+
+export default function PostsService() {
+  return {
+    async getAll(): Promise<Post[]> {
+      const PATH_POSTS = path.resolve('.', '_data', 'posts');
+      const postFiles = await fs.readdir(PATH_POSTS, { encoding: 'utf-8' });
+      const postsPromise = postFiles.map(async (postFileName) => {
+        const filePath = path.join(PATH_POSTS, postFileName);
+        const postFile = await fs.readFile(filePath, { encoding: 'utf-8' });
+        const { data, content } = matter(postFile);
+        const post: Post = {
+          metadata: {
+            date: new Date(data.date).toISOString(),
+            url: data.url,
+            excerpt: data.excerpt,
+            tags: data.tags
+          },
+          slug: postFileName.replace('.md', ''),
+          title: data.title,
+          content
+        };
+        return post;
+      });
+      const posts = Promise.all(postsPromise);
+
+      return posts;
+    }
+  };
+}
