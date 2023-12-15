2a69e17fdcba5af4366d44793b966c434c9a3708
Raí Joia <megac154@gmail.com>
2023-12-15 14:54:45 -0300

    next(api): sendGrid aplicado para o email

--- pages/api/newsletter/optin.ts
+++ pages/api/newsletter/optin.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+import sendGridMail from '@sendgrid/mail';
 import { createClient } from '@supabase/supabase-js';
 
 // Supabase setup
@@ -18,7 +19,41 @@ const httpStatus = {
 
 const controllerByMethod = {
   async POST(req: NextApiRequest, res: NextApiResponse) {
-    res.status(httpStatus.Sucess).json({ message: 'POST' });
+    const email = req.body.email;
+
+    if (!email || !email.includes('@')) {
+      res
+        .status(httpStatus.BadRequest)
+        .json({ message: 'Você precisa enviar um email válido' });
+      return;
+    }
+
+    // cadastrar no db
+    await dbClient
+      .from('newsletter_users')
+      .insert({ email: email, optin: true });
+
+    // cadastrar como user no supabase
+    await dbClient.auth.admin.createUser({ email: email });
+
+    try {
+      sendGridMail.setApiKey(process.env.SENDGRID_KEY);
+      await sendGridMail.send({
+        to: email,
+        from: 'raijoiamv@gmail.com',
+        subject: 'Newsletter - Raí Joia',
+        html: '<h1>Olá, seja bem vindo a minha newsletter!</h1>'
+      });
+
+      res
+        .status(httpStatus.Sucess)
+        .json({ message: 'email cadastrado e enviado' });
+    } catch (error) {
+      res
+        .status(httpStatus.InternalServerError)
+        .json({ message: `${error}, falhamos em enviar o seu email` });
+      return;
+    }
   },
   async GET(req: NextApiRequest, res: NextApiResponse) {
     const { data, error } = await dbClient.from('newsletter_users').select('*');
